702e6bbb23a28c1d4bd36e7bdec44d723598f84a
LCDeelen <lcdeelen@gmail.com>
2016-07-22 12:18:26 +0200

    XNA verwijderd, Unity naar root verplaatst

--- XNA/LagSpike/LagSpike/Messages/IGameMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lidgren.Network;
-public interface IGameMessage
-{
-    //GameMessageTypes MessageType { get; }
-    void Encode(NetOutgoingMessage om);
-
-    void Decode(NetIncomingMessage im);
-}
