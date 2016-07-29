11b9bf131c5223e402111921753fb42c42198954
LCDeelen <lcdeelen@gmail.com>
2016-07-29 14:28:02 +0200

    Reworked time-to-live mechanic on spikes

--- Assets/Spike/SpikeBehaviour.cs
+++ Assets/Spike/SpikeBehaviour.cs
@@ -2,25 +2,30 @@
 
 public class SpikeBehaviour : MonoBehaviour
 {
-	public float movementSpeed = 10f;
+	public float movementSpeed = 10;
+	public float secondsToLive = 5;
 
 	private Rigidbody rb;
 	private Transform tr;
-	private float originTime;
 
 	void Start()
 	{
 		rb = GetComponentInChildren<Rigidbody>();
 		tr = GetComponent<Transform>();
 
-		originTime = Time.time;
-
 		rb.velocity = movementSpeed * tr.forward.normalized;
 	}
 
 	void Update()
 	{
-		if (Time.time - originTime > 1)
+		secondsToLive -= Time.deltaTime;
+
+		if (secondsToLive < 0)
 			Destroy(gameObject);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Destroy(gameObject);
 	}
 }
