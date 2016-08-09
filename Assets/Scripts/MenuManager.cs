c7e258d0388935f38a31ca9c241ce00adbe3a1f8
LCDeelen <lcdeelen@gmail.com>
2016-08-09 23:45:12 +0200

    Menu system using https://www.youtube.com/watch?v=QxRAIjXdfFU

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+	public MenuTransitionHandler CurrentMenu;
+
+	public void Start()
+	{
+		ShowMenu(CurrentMenu);
+	}
+
+	public void ShowMenu(MenuTransitionHandler menu)
+	{
+		if (CurrentMenu != null)
+			CurrentMenu.IsOpen = false;
+
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;
+	}
+}
---
+++ Assets/Scripts/MenuTransitionHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MenuTransitionHandler : MonoBehaviour
+{
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+	public bool IsOpen
+	{
+		get { return _animator.GetBool("IsOpen"); }
+		set { _animator.SetBool("IsOpen", value); }
+	}
+
+	public void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+	}
+
+	public void Update()
+	{
+		_canvasGroup.blocksRaycasts = _canvasGroup.interactable = !_animator.GetAnimatorTransitionInfo(0).IsName("Open");
+	}
+}
