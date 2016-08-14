8b57f3448ca6377148dc462f5b6d310b3e0ba08c
LCDeelen <lcdeelen@gmail.com>
2016-08-14 20:03:06 +0200

    Play menu reworked

---
+++ Assets/Scripts/PlayMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMenuController : MonoBehaviour
+{
+	public GameObject PowerUpToggle;
+	public Slider LagSlider;
+	public InputField LagInput;
+
+	public void SliderChanged()
+	{
+		LagInput.text = ((int)LagSlider.value).ToString();
+	}
+
+	public void InputChanged()
+	{
+		try
+		{
+			LagSlider.value = int.Parse(LagInput.text);
+			LagInput.text = LagSlider.value.ToString();
+		}
+		catch { }
+	}
+}
